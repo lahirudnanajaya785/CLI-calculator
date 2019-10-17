50feb4523c5e7fc1aa282a31e9f0d3d2ad71136c
Nipun98 <55573781+ITT082@users.noreply.github.com>
2019-10-17 23:24:17 +0530

    remainder after division of one number by another
     division of one number by another

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -19,6 +19,8 @@ public class SimpleCalculator {
             return left - right;
          case '*':
             return left * right;
+         case '%':
+            return left % right;
 
        
         }
